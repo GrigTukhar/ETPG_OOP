cc988168b4c5c2f68f806848e1c386f1d9c1c136
GrigTukhar <grigor_tukharyan@edu.aua.am>
2019-10-28 14:23:26 +0400

    The proper commit way

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+#These variables should be moved coresponding classes
 
 days = ("day1", "day2", "day3", "day4", "day5", "day6", "day7")
 times = ("morning", "lunch", "day", "evening", "exit")
